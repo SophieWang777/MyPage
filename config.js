2c1319b71488eadb37a32d7dcaf3b4c90d62c4c9
Sophie Wang <sophieeew777@gmail.com>
2026-08-03 10:00:45 -0400

    Refactor text formatting in config.js
    Updated text formatting in news and education sections to remove unnecessary HTML line breaks.

--- config.js
+++ config.js
@@ -19,9 +19,9 @@ const USER_CONFIG = {
   },
 
 news: [
-    { date: "2026.07", badge: "News",  text: "Presented my poster at the<br>2026 Summer Undergraduate Research Symposium,<br>University of Notre Dame", sidelink: { url: "assets/poster_Sophie.pdf", label: "View Poster" } },
+    { date: "2026.07", badge: "News",  text: "Presented my poster at the 2026 Summer Undergraduate Research Symposium,<br>University of Notre Dame", sidelink: { url: "assets/poster_Sophie.pdf", label: "View Poster" } },
     { date: "2026.07", badge: "News",  text: "Selected as GTA (Glasgow Teaching Assistant),<br>2026 fall Introductory Programming (UESTC1005)" },
-    { date: "2026.04", badge: "News",  text: "Selected as iSURE Intern,<br>Center for Research Computing,<br>University of Notre Dame" },
+    { date: "2026.04", badge: "News",  text: "Selected as iSURE Intern,<br>Center for Research Computing,University of Notre Dame" },
     { date: "2025.09", badge: "News",  text: "Selected as GTA (Glasgow Teaching Assistant),<br>2025 fall Introductory Programming (UESTC1005)" },
     { date: "2025.06", badge: "Award", text: "Excellence Award for Short-Term Academic Exchange,<br>Glasgow College, UESTC" },
     { date: "2024.10", badge: "Award", text: "Outstanding Student Scholarship,<br>Glasgow College, UESTC" },
@@ -65,7 +65,7 @@ news: [
   ],
 
   education: [
-    { period: "2023.09–present", degree: "B.S. in Communication Engineering", institution: "Glasgow College,<br>University of Glasgow | UESTC" },
+    { period: "2023.09–present", degree: "B.S. in Communication Engineering", institution: "Glasgow College,University of Glasgow | UESTC" },
     { period: "2025.02",         degree: "Study Exchange",                    institution: "Department of Computer Science and Technology,<br>Cambridge University" },
   ],
 
